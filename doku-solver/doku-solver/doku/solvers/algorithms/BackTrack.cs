d128de37a4e97b6570c61d2f8607545ab460c8fd
Xen0Xys <red57101@gmail.com>
2022-11-22 08:54:21 +0100

    :rocket: Add backtrack solver

---
+++ doku-solver/doku-solver/doku/solvers/algorithms/BackTrack.cs
@@ -0,0 +1,37 @@
+﻿namespace doku_solver.doku.solvers.algorithms;
+
+public class BackTrack : Solver{
+
+    public override int[,] Solve(int[,] tab, int maxIterations){
+        int[,] grid = Copy(tab);
+        bool final = Backtrack(grid, 0, 0);
+        Console.WriteLine(final);
+        return grid;
+    }
+
+    private bool Backtrack(int[,] grid, int row, int column){
+        Console.WriteLine($"Input: {row}, {column}");
+        if (column > grid.GetLength(0) - 1){
+            column = 0;
+            row++;
+        }
+        if (row > grid.GetLength(1) - 1){
+            return true;
+        }
+
+        if (grid[row, column] != 0){
+            return Backtrack(grid, row, column + 1);
+        }
+        List<int> availableValues = GetSlotPossibilities(grid, row, column);
+        for (int k = 1; k <= grid.GetLength(0); k++){
+            if (availableValues.Contains(k)){
+                grid[row, column] = k;
+                if (Backtrack(grid, row, column + 1)){
+                    return true;
+                }
+            }
+        }
+        grid[row, column] = 0;
+        return false;
+    }
+}
