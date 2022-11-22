313d744e704e933d42695bf1c03f14daa6e10be2
loicmaes <97891718+loicmaes@users.noreply.github.com>
2022-11-22 14:57:53 +0100

    :tada: Init VueJS web site project

---
+++ website-improved/src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue')
+  },
+  {
+    path: '/',
+    name: 'game',
+    component: () => import('@/views/GameView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
