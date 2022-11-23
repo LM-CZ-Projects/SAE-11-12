272465bde144047b3e757e960d6e55cc6554067e
loicmaes <97891718+loicmaes@users.noreply.github.com>
2022-11-23 10:19:24 +0100

    :recycle: Move some methods to Solver

--- doku-solver/doku-solver/doku/solvers/algorithms/OtherBackTrack.cs
+++ doku-solver/doku-solver/doku/solvers/algorithms/OtherBackTrack.cs
@@ -7,38 +7,6 @@ public class OtherBackTrack: Solver {
         SmartSolve(tab);
         return tab;
     }
-    
-    private bool IsInRow(int[,] grid, int number, int row) {
-        for (int i = 0; i < GRID_SIZE; i++)
-            if (grid[row, i] == number)
-                return true;
-        return false;
-    }
-
-    private bool IsInColumn(int[,] grid, int number, int column) {
-        for (int i = 0; i < GRID_SIZE; i++)
-            if (grid[i, column] == number)
-                return true;
-        return false;
-    }
-
-    private bool IsInSection(int[,] grid, int number, int row, int column) {
-        int boxSize = (int) Math.Sqrt(GRID_SIZE);
-        int localRow = row - row % boxSize;
-        int localColumn = column - column % boxSize;
-        
-        for (int i = localRow; i < localRow + boxSize; i++)
-        for (int j = localColumn; j < localColumn + boxSize; j++)
-            if (grid[i, j] == number)
-                return true;
-        return false;
-    }
-
-    private bool IsValidPlacement(int[,] grid, int number, int row, int column) {
-        return !IsInRow(grid, number, row) &&
-               !IsInColumn(grid, number, column) &&
-               !IsInSection(grid, number, row, column);
-    }
 
     private bool SmartSolve(int[,] grid) {
         for (int i = 0; i < GRID_SIZE; i++) {
