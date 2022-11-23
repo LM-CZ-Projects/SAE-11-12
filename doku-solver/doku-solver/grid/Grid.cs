8daa0bddcf54987220680501b6ccc61ff64ca370
Xen0Xys <red57101@gmail.com>
2022-11-23 09:07:59 +0100

    :rocket: Add a method to copy array in Grid object

--- doku-solver/doku-solver/grid/Grid.cs
+++ doku-solver/doku-solver/grid/Grid.cs
@@ -7,11 +7,18 @@ public class Grid{
     private Cursor Cursor{ get; }
 
     public Grid(int[,] grid){
-        this.SquareGrid = grid;
+        SquareGrid = CopyArray(grid);
         Cursor = new Cursor(0, 0, this.SquareGrid.GetLength(0));
     }
-    
-    
-    
+
+    private int[,] CopyArray(int[,] array){
+        int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
+        for(int i = 0; i < array.GetLength(0); i++){
+            for(int j = 0; j < array.GetLength(1); j++){
+                newArray[i, j] = array[i, j];
+            }
+        }
+        return newArray;
+    }
     
 }
