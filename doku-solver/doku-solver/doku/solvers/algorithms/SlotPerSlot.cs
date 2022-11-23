da09f8d963a1c6091d9b5b646d742ff89e38e2c7
Xen0Xys <red57101@gmail.com>
2022-11-23 10:04:02 +0100

    :rocket: Implement SlotPerSlot.cs algorithm with new Grid object

--- doku-solver/doku-solver/doku/solvers/algorithms/SlotPerSlot.cs
+++ doku-solver/doku-solver/doku/solvers/algorithms/SlotPerSlot.cs
@@ -1,22 +1,27 @@
-﻿namespace doku_solver.doku.solvers.algorithms;
+﻿using doku_solver.doku.tools;
+using doku_solver.grid;
+
+namespace doku_solver.doku.solvers.algorithms;
 
 public class SlotPerSlot : Solver{
     public override int[,] Solve(int[,] tab, int maxIterations){
-        int[,] result = Copy(tab);
+        Grid grid = new Grid(tab);
         int iterations = 0;
-        while(!IsSolved(result) && iterations < maxIterations){
-            for (int i = 0; i < result.GetLength(0); i++){
-                for (int j = 0; j < result.GetLength(1); j++){
-                    if (result[i, j] == 0){
-                        List<int> possibilities = GetSlotPossibilities(result, i, j);
-                        if (possibilities.Count == 1){
-                            result[i, j] = possibilities[0];
-                        }
+        while(!IsSolved(grid) && iterations < maxIterations){
+            while (grid.Cursor.HasNext()){
+                Position position = grid.Cursor.GetPosition();
+                // Console.WriteLine($"{position.Row}, {position.Column}");
+                if (grid.GetOnCursor() == 0){
+                    List<int> possibilities = GetSlotPossibilities(grid, position);
+                    if (possibilities.Count == 1){
+                        grid.SetOnCursor(possibilities[0]);
                     }
                 }
+                grid.Cursor.Next();
             }
+            grid.Cursor.Reset();
             iterations++;
         }
-        return result;
+        return grid.GetGrid();
     }
 }
