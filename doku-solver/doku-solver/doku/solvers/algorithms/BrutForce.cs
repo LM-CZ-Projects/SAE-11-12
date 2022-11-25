2fa26f4f3f2c0be5b77e860d16a2e5658e5a992b
loicmaes <97891718+loicmaes@users.noreply.github.com>
2022-11-25 15:14:17 +0100

    :rocket: New algorithms (brutefore and random bruteforce)

--- doku-solver/doku-solver/doku/solvers/algorithms/BrutForce.cs
+++
@@ -1,9 +0,0 @@
-﻿using doku_solver.grid;
-
-namespace doku_solver.doku.solvers.algorithms;
-
-public class BrutForce : Solver{
-    public override Grid Solve(Grid grid, int maxIterations){
-        throw new NotImplementedException();
-    }
-}
