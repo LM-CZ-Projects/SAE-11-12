b7256a1389a3d1a87386a39eae9a79cf7ab655b7
Xen0Xys <red57101@gmail.com>
2022-11-25 14:55:03 +0100

    :rocket: Optimize generation algorithm

--- doku-solver/doku-solver/DokuSolver.cs
+++ doku-solver/doku-solver/DokuSolver.cs
@@ -5,11 +5,13 @@ namespace doku_solver;
 
 public static class DokuSolver{
     public static void Main(){
-        new Tester().TestGeneratorPerformances(1000, 3, false, true); // 12.7s
+        GridIO.GenerateExportJsonGrids(100000, 3, "tests_10k_3x3");
+        // new Tester().TestGeneratorPerformances(100000, 3, false, true); // 12.7s
     }
     
     public static void DisplayGrid(Grid grid){
         Console.WriteLine("-----------------");
+        grid.Cursor.Reset();
         while (grid.Cursor.HasNext()){
             if (grid.Cursor.GetPosition().Column == 0) Console.WriteLine();
             Console.Write(grid.GetOnCursor() + " ");
